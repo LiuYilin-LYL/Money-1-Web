56e462a0f9405e51861efd5332a6e928e7139cda
Yilin <1012455587@qq.com>
2021-01-29 21:15:45 +0800

    deploy

---
+++ precache-manifest.7cf5bfb521575281378fd296bdb3f741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1a4c5861c16016de4f",
+    "url": "/Money-1-Web/css/app.d1010cb7.css"
+  },
+  {
+    "revision": "dc4699c6611e0a4210fc85b31023fef1",
+    "url": "/Money-1-Web/index.html"
+  },
+  {
+    "revision": "5d1a4c5861c16016de4f",
+    "url": "/Money-1-Web/js/app.a93dffd1.js"
+  },
+  {
+    "revision": "fd618cf4f3f8285e6035",
+    "url": "/Money-1-Web/js/chunk-vendors.b8987963.js"
+  },
+  {
+    "revision": "a4a420f1861a49a945a87ab7ddc06254",
+    "url": "/Money-1-Web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-1-Web/robots.txt"
+  }
+]);
